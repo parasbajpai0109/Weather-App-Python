874adf85cf7e9f96a1c46de2dc78c42ac4d7b9bb
parasbajpai0109 <12345678+parasbajpai0109@users.noreply.github.com>
2026-06-09 13:22:08 +0530

    Initial commit- Weather-App

---
+++ weatherapp.py
@@ -0,0 +1,47 @@
+import requests
+while True:
+    print("       Welcome to Weather-App")
+    city = input("Enter City Name (eg: Delhi, India) ('q' for Exit): ")
+
+    if city.lower() == 'q':
+        print("Thanks for using Weather-app")
+        break
+
+    api_key = "f991d5e1de644521a38103227260806"
+    url =f"https://api.weatherapi.com/v1/current.json?key={api_key}&q={city}"
+
+    response = requests.get(url)
+    data = response.json()
+
+    if "error" in data:
+        print("City Not Found")
+
+    else:
+        print(f"       Weather report of {city}")
+        city_name = data['location']['name']
+        print(f"City Name: {city_name}")
+
+        region_name = data['location']['region']
+        print(f"Region Name: {region_name}")
+
+        country_name = data['location']['country']
+        print(f"Country Name: {country_name}")
+
+        local_time = data['location']['localtime']
+        print(f"Local Time {local_time}")
+
+        temp = data['current']['temp_c']
+        print(f"Temperature: {temp}°C")
+
+        feels_like = data['current']['feelslike_c']
+        print(f"Feels Like: {feels_like}°C")
+
+        humidity = data['current']['humidity']
+        print(f"Humidity: {humidity}%")
+
+        wind_speed = data['current']['wind_kph']
+        print(f"Wind Speed: {wind_speed} km/h")
+
+        condition = data['current']['condition']['text']
+        print(f"Condition: {condition}")
+
